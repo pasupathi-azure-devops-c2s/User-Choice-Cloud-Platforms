40e5a0158389b2393befc0afdcc94d5f728526c4
Pasupathikumar <kumarmarimuthu99@gmail.com>
2024-12-10 13:17:13 +0530

    sa

--- Serverless-Engine/AWS-Engine/AWS-Engine.tf
+++ Serverless-Engine/AWS-Engine/AWS-Engine.tf
@@ -18,6 +18,34 @@ resource "aws_vpc" "serverless-vpc" {
   
 }
 
-resource "aws_apprunner_service" "name" {
+resource "aws_subnet" "serverless-subnet" {
+    vpc_id = aws_vpc.serverless-vpc.id
+    cidr_block = var.subent-cidr
+    availability_zone = "us-west-2a"
+    map_public_ip_on_launch = true
   
+}
+
+resource "aws_apprunner_service" "name" {
+    service_name = var.resource_name
+
+    source_configuration {
+      authentication_configuration {
+        connection_arn = aws_apprunner_connection.apprunner-connection.arn
+
+      }
+
+      image_repository {
+          image_identifier      = aws_ecr_repository.serverless-engine-aws_ecr_repository.repository_url
+          image_repository_type = "ECR"  # Specifies that the source is ECR
+    }
+
+    }
+
+    network_configuration {
+      egress_configuration {
+        egress_type = "VPC"
+        vpc_connector_arn = aws_apprunner_vpc_connector.vpc-connector.arn
+      }
+    }
 }
